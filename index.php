1b3e0311243282b98e769d4197a5bd69ca0c431c
soporte <soporte@indexcode.com.co>
2016-12-21 17:52:57 -0500

    Primera parte plantilla

--- index.php
+++ index.php
@@ -6,70 +6,115 @@
         <title>Marvel - Developer</title>
         <link rel="stylesheet" href="assets/global/bootstrap-3.3.7-dist/css/bootstrap.min.css"/>
         <link rel="stylesheet" href="assets/global/bootstrap-3.3.7-dist/css/bootstrap-theme.min.css"/>
-        
+        <link rel="stylesheet" href="assets/global/simple-line-icons/css/simple-line-icons.css"/>
+
         <style type="text/css">
-            nav{
-                background-color: #2D2727;
+            body{
+                background-color: #F4F4F4;
             }
+
+            .form-control{
+                background-color: #fff;
+                border: 1px solid #e5e5e5;
+                border-radius: 3px;
+                box-shadow: none;
+                color: #333;
+                font-size: 14px;
+                font-weight: normal;
+                transition: border-color 0.15s ease-in-out 0s, box-shadow 0.15s ease-in-out 0s;
+            }
+
+            .navbar {
+                margin-bottom: 0px;
+                -webkit-box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.4);
+                -moz-box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.4);
+                box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.4);
+                z-index: 10;
+            }
+            .navbar-default{
+                background-image: inherit;
+                background-color: #2D2727;
+                border: 0px;
+                border-radius: 0px;
+                height: 80px;
+            }
+
+            #logo{
+                width: 170px;
+            }
+
+            .navTop{
+                padding-top: 10px; 
+            }
+
+            #search{
+                padding: 5px;
+            }
+
+            #search input{
+                height: 50px;
+                background-color: #FAFAFA;
+            }
+
+            .form-horizontal .has-feedback .form-control-feedback {
+                color: #b5b5b5;
+                font-size: 22pt;
+                right: 15px;
+                top: 15px;
+                vertical-align: middle;
+            }
+            
+            .section{
+                padding-top: 30px;
+            }
+            
+            .titleIcon{
+                margin-top: 3px; 
+                font-weight: bold
+            }
+            
         </style>
     </head>
     <body>
+
         <nav class="navbar navbar-default">
             <div class="container-fluid">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Brand</a>
-                </div>
-
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                        <li><a href="#">Link</a></li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="#">Separated link</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="#">One more separated link</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <form class="navbar-form navbar-left">
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Search">
+                <div class="navTop">
+                    <form class="form-horizontal" role="search">
+                        <div class="form-group has-feedback">
+                            <div class="col-xs-3">
+                                <img alt="Marvel" id="logo" src="assets/img/Marvel-logo.png">
+                            </div>
+                            <div class="col-xs-6" id="search">
+                                <input type="text" class="form-control" id="inputSuccess3" placeholder="Search character..." aria-describedby="searchMarvel">
+                                <span class="icon-magnifier icons form-control-feedback" aria-hidden="true"></span>
+                            </div>
                         </div>
-                        <button type="submit" class="btn btn-default">Submit</button>
                     </form>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#">Link</a></li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="#">Separated link</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div><!-- /.navbar-collapse -->
-            </div><!-- /.container-fluid -->
+                </div>
+            </div>
         </nav>
 
-
-
+        <div class="container-fuid">
+                <div class="col-md-9 col-sm-12 section">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <img class="img-responsive pull-left " src="assets/icons/characters.png" alt="characters">
+                            <h2 class="pull-left titleIcon">Characters</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 hidden-sm section" style="background-color: #EBEBEB;">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <img class="img-responsive center-block" src="assets/icons/favourites.png" alt="favourites">
+                        </div>
+                        <div class="col-sm-9">
+                            <h2 class="titleIcon">My favourites</h2>
+                        </div>
+                    </div>
+                </div>
+        </div>
 
         <!-- Librerias -->
         <script type="text/javascript" src="assets/global/jquery-3.1.1.min.js"></script>
