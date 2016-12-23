9720095b046d419515a9279af9da1590441ecc5a
soporte <soporte@indexcode.com.co>
2016-12-23 15:52:33 -0500

    Comic

--- assets/js/jq_marvel.js
+++ assets/js/jq_marvel.js
@@ -5,7 +5,8 @@ $(function () {
     //------------------------------------------------------------------------//
     var _publicKey = "e680285ab76f2b29840abc583f971ba9";
     var _url = 'https://gateway.marvel.com/v1/';
-    
+    var _AddComics = [];
+
     //------------------------------------------------------------------------//
     //  Method's init
     //------------------------------------------------------------------------//
@@ -22,8 +23,89 @@ $(function () {
     //------------------------------------------------------------------------//
     //  Eventos
     //------------------------------------------------------------------------//
-    $("#clickMe").click(function () {
+    $('#mdlComicCharacter').on('show.bs.modal', function (event) {
 
+        // Var
+        var button = $(event.relatedTarget); // Button that triggered the modal
+        var comic = button.data('comic'); // Extract info from data-* attributes
+        var modal = $(this);
+
+        // Events
+        $("#bodyMarvel").css("filter", "blur(20px)");
+
+        // Get
+        var sUrl = comic;
+        var aDataGet = {apikey: _publicKey};
+
+        $.get(sUrl, aDataGet).done(function (msg) {
+            var aData = msg.data.results[0];
+
+            var img = aData.thumbnail.path + "." + aData.thumbnail.extension;
+            var title = aData.title;
+            var button2 = "<img src='assets/icons/shopping-cart-primary.png' alt='Buy' /> BUY FOR $" + aData.prices[0].price;
+
+            modal.find("h2").text(aData.title);
+            modal.find("p").text(aData.description);
+            modal.find("img").eq(1).attr("src", img);
+            modal.find("button").eq(1).data("comic", aData.id).data("img", img).data("title", title);
+            modal.find("button").eq(2).html(button2);
+        }).fail(function (err) {
+            console.log(err);
+        });
+
+    });
+
+    // Hide fond blur modal
+    $('#mdlComicCharacter').on('hide.bs.modal', function (event) {
+        // Events
+        $("#bodyMarvel").css("filter", "blur(0px)");
+    });
+
+    $("#btnAddFavourites").click(function () {
+        var id = $(this).data("comic");
+        var img = $(this).data("img");
+        var title = $(this).data("title");
+        
+        _AddComics.push({id:id, title: title, img: img}); 
+        
+        var hContainer = "";
+        hContainer += "<div class='containerComic'>";
+        hContainer += "<div class='row'>";
+        hContainer += "<div class='col-xs-12'>";
+        hContainer += "<img src='assets/icons/btn-delete.png' class='btnDelete' data-comic='" + id + "'/>";
+        hContainer += "<img class='img-responsive' src='" + img + "' alt='comic'>";
+        hContainer += "</div>";
+        hContainer += "</div>";
+        // Description
+        hContainer += "<div class='row'>";
+        hContainer += "<div class='col-xs-12'>";
+        hContainer += "<h3 class='text-center'>" + title + "</h3>";
+        hContainer += "</div>";
+        hContainer += "</div>";
+        hContainer += "</div>";
+        hContainer += "<br />";
+        
+        $("#container-comics").append(hContainer);
+        
+        // Hide Modal
+        $("#mdlComicCharacter").modal("hide");
+        
+        console.log(_AddComics);
+    });
+
+    $("#container-comics").on("click", ".btnDelete", function(){
+        var delComic = $(this).data("comic");
+        
+        $(this).parent().parent().parent().remove();
+        
+//        $("#container-comics").empty();
+//        $.each(_AddComics, function(nIndice, aData){
+//            if(aData.id == delComic){
+//                _AddComics.splice(nIndice);
+//                return true
+//            }
+//        });
+        
     });
 
     //------------------------------------------------------------------------//
@@ -33,12 +115,12 @@ $(function () {
 
         // Params Get
         var sUrl = _url + "public/characters";
-        var aData = {apikey: _publicKey, limit: 10, offset: offset};
+        var aDataGet = {apikey: _publicKey, limit: 10, offset: offset};
 
         // Clear Container
         $("#container-characters").empty();
 
-        $.get(sUrl, aData).done(function (msg) {
+        $.get(sUrl, aDataGet).done(function (msg) {
             var nRow = 0;
             var hContainer = "";
 
@@ -65,7 +147,7 @@ $(function () {
                     sDescription = "- No description -";
                 }
                 hContainer += "<p>" + sDescription + "</p>";
-                hContainer += "<button type='button' class='btn' data-character='" + aData.id + "'>VIEW MORE</button>";
+                hContainer += "<a type='button' href='"+aData.urls[0].url+"' target='_blank' class='btn' data-character='" + aData.id + "'>VIEW MORE</a>";
                 hContainer += "</div>";
                 hContainer += "</div>";
 
@@ -80,7 +162,7 @@ $(function () {
 
                     // Print comics
                     hContainer += "<div class='col-xs-12 col-sm-6'>";
-                    hContainer += "<p>" + $aDataComics.name + "</p>";
+                    hContainer += "<a href='#' data-toggle='modal' data-target='#mdlComicCharacter' data-comic='" + $aDataComics.resourceURI + "'>" + $aDataComics.name + "</a>";
                     hContainer += "</div>";
 
                     // Close Row Comics
@@ -112,6 +194,45 @@ $(function () {
 
             $("#container-characters").html(hContainer);
         }).fail(function (err) {
+            console.log(err);
+        });
+    }
+
+    function printComics() {
+        // Params Get
+        var sUrl = _url + "public/comics";
+        var aDataGet = {apikey: _publicKey, limit: 4, offset: 0};
+
+
+        // Clear Container Comic
+        $("#container-comics").empty();
+
+        var hContainer = "";
+        $.get(sUrl, aDataGet).done(function (msg) {
+
+            // Run Comic
+            $.each(msg.data.results, function (nIndice, aData) {
+                // Image
+                hContainer += "<div class='row'>";
+                hContainer += "<div class='col-xs-12'>";
+                hContainer += "<img src='assets/icons/btn-delete.png' class='btnDelete' data-comic='" + aData.id + "'/>";
+                hContainer += "<img class='img-responsive' src='" + aData.thumbnail.path + "." + aData.thumbnail.extension + "' alt='comic'>";
+                hContainer += "</div>";
+                hContainer += "</div>";
+                // Description
+                hContainer += "<div class='row'>";
+                hContainer += "<div class='col-xs-12'>";
+                hContainer += "<h3 class='text-center'>" + aData.title + "</h3>";
+                hContainer += "</div>";
+                hContainer += "</div>";
+                hContainer += "<br />";
+            });
+
+            $("#container-comics").html(hContainer);
+
+
+        }).fail(function (err) {
+            alert("Ango anda mal");
             console.log(err);
         });
     }
--- index.php
+++ index.php
@@ -9,24 +9,24 @@
         <link rel="stylesheet" href="assets/global/font-awesome/css/font-awesome.min.css">
         <link rel="stylesheet" href="assets/global/simple-line-icons/simple-line-icons.min.css"/>
         <link rel="stylesheet" href="assets/global/bootstrap-select/bootstrap-select.min.css"/>
-        
+
         <!-- Page -->
         <link rel="stylesheet" href="assets/css/components.css"/>
         <link rel="stylesheet" href="assets/css/plugins.css "/>
 
         <style type="text/css">
-            
+
             /* Font */
             @font-face {
                 font-family: TradeGothicBold;
                 src: url(assets/fonts/trade-gothic-bold/TradeGothic-Bold.woff);
             }
-            
+
             /* Style */
             body{
                 background-color: #F4F4F4;
             }
-            
+
             h1, h2, h3, h4, h5, h6 {
                 font-family: TradeGothicBold;
             }
@@ -83,66 +83,124 @@
                 top: 22px;
                 vertical-align: middle;
             }
-            
+
+            #bodyMarvel{
+                display: flex;
+            }
+
             .section{
                 padding-top: 30px;
             }
-            
+
             .titleIcon{
                 margin-top: 3px; 
                 font-weight: bold;
                 font-family: TradeGothicBold;
             }
-            
+
             .selectpicker{
                 height: 50px;
             }
-            
+
             .cantainer-character{
                 background-color: #FFF;
                 padding: 0 15px;
                 display: block;
                 margin: 20px 0px 20px 20px;
             }
-            
+
             .cantainer-character .characterImage{
                 left: -30px;
                 top: -15px;
                 padding: 0px;
             }
-            
+
             .cantainer-character .characterImage img{
                 height: 22rem;
                 width: 24rem;
             }
-            
+
             .cantainer-character .characterDescription{
                 top: -36px;
                 padding: 0;
             }
-            
-            .cantainer-character p{
+
+            .cantainer-character p, .cantainer-character a{
                 font-size: 14px;
                 color: #B9B3B3;
             }
-            
-            .cantainer-character .characterDescription button{
+
+            .cantainer-character .characterDescription a{
                 background-color: #EB1E23;
                 color: #FFF;
                 font-weight: bold;
             }
-            
+
             .characterHead{
                 padding: 0 10px;
                 min-height: 260px;
             }
-            
+
             .characterBody{
                 position: relative;
                 top: -30px;
                 min-height: 135px;
             }
+
+            .btnDelete{
+                position: absolute;
+                right: 0;
+                top: -20px;
+            }
+
+            /* Modal */
+            .modal-backdrop.in{
+                opacity: 0.7 !important;
+            }
             
+            .modal-dialog {
+                width: 700px;
+            }
+            
+            
+            .modal-body{
+                padding: 5px 15px 15px 15px;
+            }
+            
+            .modal-body h2{
+                margin-top: 0px;
+            }
+            
+            .modal-body p{
+                color: #b9b3b3;
+                font-size: 14px;
+            }
+            
+            .modal-footer{
+                display: flex;
+                padding: 0;
+            }
+            
+            .modal-footer button{
+                flex: 1 1 auto;
+                margin: 0 !important;
+                font-weight: bold;
+                padding: 10px 0;
+            }
+            
+            .modal-footer button:nth-child(1){
+                background-color:  #F0F0F0; 
+                color: #3C3332;
+            }
+            .modal-footer button:nth-child(1):hover{
+                background-color:  #322827; 
+                color: #EC1D23;
+            }
+            .modal-footer button:nth-child(2){
+                background-color:  #DCDCDC; 
+                color: #EC1D23;
+            }
+
         </style>
     </head>
     <body>
@@ -165,39 +223,77 @@
             </div>
         </nav>
 
-        <div class="container-fuid">
-                <div class="col-md-9 col-sm-12 section">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <img class="img-responsive pull-left " src="assets/icons/characters.png" alt="characters">
-                            <h2 class="pull-left titleIcon">Characters</h2>
-                        </div>
-                        <div class="col-sm-offset-2 col-sm-4">
-                            <select class="form-control bs-select" name="sortBy" id="sortBy">
-                                <option value="">Sort By</option>
-                                <option value="">Sort By1</option>
-                                <option value="">Sort By2</option>
-                                <option value="">Sort By3</option>
-                                <option value="">Sort By4</option>
-                                <option value="">Sort By5</option>
-                                <option value="">Sort By6</option>
-                            </select>
-                        </div>
+        <div class="container-fuid" id="bodyMarvel">
+            <div class="col-md-9 col-sm-12 section">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <img class="img-responsive pull-left " src="assets/icons/characters.png" alt="characters">
+                        <h2 class="pull-left titleIcon">Characters</h2>
                     </div>
-                    <br />
-                    <div id="container-characters">
+                    <div class="col-sm-offset-2 col-sm-4">
+                        <select class="form-control bs-select" name="sortBy" id="sortBy">
+                            <option value="">Sort By</option>
+                            <option value="">Sort By1</option>
+                            <option value="">Sort By2</option>
+                            <option value="">Sort By3</option>
+                            <option value="">Sort By4</option>
+                            <option value="">Sort By5</option>
+                            <option value="">Sort By6</option>
+                        </select>
                     </div>
                 </div>
-                <div class="col-md-3 hidden-sm section" style="background-color: #EBEBEB;">
-                    <div class="row">
-                        <div class="col-sm-3">
-                            <img class="img-responsive center-block" src="assets/icons/favourites.png" alt="favourites">
-                        </div>
-                        <div class="col-sm-9">
-                            <h2 class="titleIcon">My favourites</h2>
-                        </div>
+                <br />
+                <div id="container-characters">
+                </div>
+            </div>
+            <div class="col-md-3 hidden-sm section" style="background-color: #EBEBEB;">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <img class="img-responsive center-block" src="assets/icons/favourites.png" alt="favourites">
+                    </div>
+                    <div class="col-sm-9">
+                        <h2 class="titleIcon">My favourites</h2>
                     </div>
                 </div>
+                <br />
+                <div id="container-comics">
+                </div>
+            </div>
+        </div>
+
+        <!-- Modal Character -->
+        <div class="modal fade" id="mdlComicCharacter" tabindex="-1" role="dialog" aria-labelledby="characterModal">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <button type="button" class="btn btn-link pull-right" data-dismiss="modal">
+                                    <img src="assets/icons/btn-close.png" alt="Close" />
+                                </button>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-5">
+                                <img class="img-responsive" src="" alt="comic">
+                            </div>
+                            <div class="col-xs-7">
+                                <h2></h2>
+                                <p>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn" id="btnAddFavourites">
+                            <img src="assets/icons/btn-favourites-default.png" alt="Favourites" /> 
+                            ADD TO FAVOURITES
+                        </button>
+                        <button class="btn">
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <!-- Librerias -->
