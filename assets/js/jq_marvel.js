2a0a7ef19a0c4e53dbe09f0927482325d3ba533d
soporte <soporte@indexcode.com.co>
2016-12-23 22:59:12 -0500

    Comic

--- assets/js/jq_marvel.js
+++ assets/js/jq_marvel.js
@@ -6,11 +6,45 @@ $(function () {
     var _publicKey = "e680285ab76f2b29840abc583f971ba9";
     var _url = 'https://gateway.marvel.com/v1/';
     var _AddComics = [];
+    var _AddCharacters = [];
+    var _sShearch = getParameterByName("search") || "";
+    var _sOffSet = getParameterByName("offset") || 1;
 
     //------------------------------------------------------------------------//
     //  Method's init
     //------------------------------------------------------------------------//
-    printCharacters(0);
+
+    // Search
+    $("#search").val(_sShearch);
+
+    // Offset
+    if (Math.floor(_sOffSet) != _sOffSet || !$.isNumeric(_sOffSet)) {
+        alert("Paginator no integer");
+        window.location = "index.php";
+        return false;
+    }
+
+    // Local Storage
+    if (localStorage["_AddComics"]) {
+        _AddComics = JSON.parse(localStorage["_AddComics"]);
+        $.each(_AddComics, function (nIndice, aData) {
+            printComic(aData.id, aData.title, aData.img);
+        });
+    }
+
+    if (localStorage["_AddCharacters"] && _sShearch == "" && _sOffSet == "") {
+        _AddCharacters = JSON.parse(localStorage["_AddCharacters"]);
+        if (_AddCharacters.data.results.length > 1) {
+            printCharacters(_AddCharacters);
+        } else {
+            getPrintCharacters(_sOffSet);
+        }
+    } else {
+        getPrintCharacters(_sOffSet);
+    }
+
+
+
 
     //------------------------------------------------------------------------//
     //  Eventos Librerias
@@ -57,17 +91,190 @@ $(function () {
 
     // Hide fond blur modal
     $('#mdlComicCharacter').on('hide.bs.modal', function (event) {
-        // Events
-        $("#bodyMarvel").css("filter", "blur(0px)");
+        $("#bodyMarvel").css("filter", "blur(0px)"); // Clear filter
     });
 
     $("#btnAddFavourites").click(function () {
         var id = $(this).data("comic");
         var img = $(this).data("img");
         var title = $(this).data("title");
+        var bComic = true;
+
+        // Comic no repead
+        $.each(_AddComics, function (nIndice, aData) {
+            if (aData.id == id) {
+                alert("Comic already selected");
+                bComic = false;
+                return false;
+            }
+        });
+        if (!bComic)
+            return false;
+
+        // Set Items Comic
+        if (_AddComics.length < 3) {
+
+            _AddComics.push({id: id, title: title, img: img});
+            localStorage.setItem("_AddComics", JSON.stringify(_AddComics));
+
+            // Print Comic
+            printComic(id, title, img);
+
+            // Hide Modal
+            $("#mdlComicCharacter").modal("hide");
+
+
+        } else {
+            alert("You can only select 3 comics");
+        }
+    });
+
+    // Delete Comic
+    $("#container-comics").on("click", ".btnDelete", function () {
+        var delComic = $(this).data("comic");
+
+        // Remove Html
+        $(this).parent().parent().parent().remove();
+
+        for (var aData in _AddComics) {
+            if (_AddComics[aData].id == delComic) {
+                _AddComics.splice(aData, 1);
+            }
+        }
+
+        // Update LocalStorage
+        localStorage.setItem("_AddComics", JSON.stringify(_AddComics));
+
+    });
+
+
+    //------------------------------------------------------------------------//
+    //  Method's
+    //------------------------------------------------------------------------//
+
+    function getPrintCharacters(offset) {
+
+        // Params localStorage
+        var sSetOffSet = (offset - 1) + "0";
+
+        // Params Get
+        var sUrl = _url + "public/characters";
+        var aDataGet = {apikey: _publicKey, limit: 10, offset: sSetOffSet};
+
+        // Valid
+        if (_sShearch != "")
+            aDataGet["name"] = _sShearch;
+
+        // Clear Container
+        $("#container-characters").empty();
+
+        $.get(sUrl, aDataGet).done(function (msg) {
+
+            // Save LocalStorage
+            localStorage.setItem("_AddCharacters", JSON.stringify(msg));
+            printCharacters(msg);
+
+        }).fail(function (err) {
+            console.log(err);
+        });
+    }
+
+    function printCharacters(msg) {
+        // Var Character
+        var nRow = 0;
+        var hContainer = "";
         
-        _AddComics.push({id:id, title: title, img: img}); 
-        
+        var items = (msg.data.total / 10);
+
+        // Config Navigation
+        $('#pageNavigation').pagination({
+            items: items,
+            itemOnPage: 2,
+            edges: 2,
+            currentPage: _sOffSet,
+            prevText: "<i class='fa fa-angle-left' aria-hidden='true'></i>",
+            nextText: "<i class='fa fa-angle-right' aria-hidden='true'></i>",
+            onInit: function () {
+                $("#pageNavigation").find("ul").addClass("pagination").addClass("pagination-lg");
+            },
+            onPageClick: function (page, evt) {
+                $("#pageNavigation").find("ul").addClass("pagination").addClass("pagination-lg");
+                window.location = "index.php?offset=" + page;
+            }
+        });
+
+        // Run Characters
+        $.each(msg.data.results, function (nIndice, aData) {
+            // Open Row
+            hContainer += (nRow++ == 0) ? "<div class='row'>" : "";
+
+            // Open Container
+            hContainer += "<div class='col-xs-12 col-sm-6'>"; // Reserved columns to character
+            hContainer += "<div class='cantainer-character'>"; // Container character
+
+            // Head
+            hContainer += "<div class='row characterHead'>";
+            hContainer += "<div class='col-xs-12 col-sm-6 characterImage'>";
+            hContainer += "<img class='img-responsive img-circle' src='" + aData.thumbnail.path + "." + aData.thumbnail.extension + "' alt='Character' />";
+            hContainer += "</div>";
+            hContainer += "<div class='col-xs-12 col-sm-6 characterDescription'>";
+            hContainer += "<h3>" + aData.name + "</h3>";
+            var sDescription = aData.description;
+            if (sDescription.length >= 217) {
+                sDescription = sDescription.substring(0, 217) + " ...";
+            } else if (sDescription.length == 0) {
+                sDescription = "- No description -";
+            }
+            hContainer += "<p>" + sDescription + "</p>";
+            hContainer += "<a type='button' href='" + aData.urls[0].url + "' target='_blank' class='btn' data-character='" + aData.id + "'>VIEW MORE</a>";
+            hContainer += "</div>";
+            hContainer += "</div>";
+
+            // Body
+            hContainer += "<div class='characterBody'>";
+            hContainer += "<h4>Related comics</h4>";
+            // Body Print 4 comincs
+            var nRowComic = 0;
+            $.each(aData.comics.items, function (nIndiceComics, $aDataComics) {
+                // Open Row Comics
+                hContainer += (nRowComic++ == 0) ? "<div class='row'>" : "";
+
+                // Print comics
+                hContainer += "<div class='col-xs-12 col-sm-6'>";
+                hContainer += "<a href='#' data-toggle='modal' data-target='#mdlComicCharacter' data-comic='" + $aDataComics.resourceURI + "'>" + $aDataComics.name + "</a>";
+                hContainer += "</div>";
+
+                // Close Row Comics
+                if (nRowComic == 2) {
+                    hContainer += "</div>";
+                    nRowComic = 0;
+                }
+
+                if (nIndiceComics == 3)
+                    return false; // Close bucle
+            });
+            // Close Row number odd
+            hContainer += (nRowComic > 0) ? "</div>" : "";
+            hContainer += "</div>"; // End body Comics
+
+            // End Container
+            hContainer += "</div>";
+            hContainer += "</div>";
+
+            // Close Row
+            if (nRow == 2) {
+                hContainer += "</div>";
+                nRow = 0;
+            }
+        });
+
+        // Close Row number odd
+        hContainer += (nRow > 0) ? "</div>" : "";
+
+        $("#container-characters").html(hContainer);
+    }
+
+    function printComic(id, title, img) {
         var hContainer = "";
         hContainer += "<div class='containerComic'>";
         hContainer += "<div class='row'>";
@@ -84,157 +291,21 @@ $(function () {
         hContainer += "</div>";
         hContainer += "</div>";
         hContainer += "<br />";
-        
+
         $("#container-comics").append(hContainer);
-        
-        // Hide Modal
-        $("#mdlComicCharacter").modal("hide");
-        
-        console.log(_AddComics);
-    });
-
-    $("#container-comics").on("click", ".btnDelete", function(){
-        var delComic = $(this).data("comic");
-        
-        $(this).parent().parent().parent().remove();
-        
-//        $("#container-comics").empty();
-//        $.each(_AddComics, function(nIndice, aData){
-//            if(aData.id == delComic){
-//                _AddComics.splice(nIndice);
-//                return true
-//            }
-//        });
-        
-    });
-
-    //------------------------------------------------------------------------//
-    //  Method's
-    //------------------------------------------------------------------------//
-    function printCharacters(offset) {
-
-        // Params Get
-        var sUrl = _url + "public/characters";
-        var aDataGet = {apikey: _publicKey, limit: 10, offset: offset};
-
-        // Clear Container
-        $("#container-characters").empty();
-
-        $.get(sUrl, aDataGet).done(function (msg) {
-            var nRow = 0;
-            var hContainer = "";
-
-            // Run Characters
-            $.each(msg.data.results, function (nIndice, aData) {
-                // Open Row
-                hContainer += (nRow++ == 0) ? "<div class='row'>" : "";
-
-                // Open Container
-                hContainer += "<div class='col-xs-12 col-sm-6'>"; // Reserved columns to character
-                hContainer += "<div class='cantainer-character'>"; // Container character
-
-                // Head
-                hContainer += "<div class='row characterHead'>";
-                hContainer += "<div class='col-xs-12 col-sm-6 characterImage'>";
-                hContainer += "<img class='img-responsive img-circle' src='" + aData.thumbnail.path + "." + aData.thumbnail.extension + "' alt='Character' />";
-                hContainer += "</div>";
-                hContainer += "<div class='col-xs-12 col-sm-6 characterDescription'>";
-                hContainer += "<h3>" + aData.name + "</h3>";
-                var sDescription = aData.description;
-                if (sDescription.length >= 217) {
-                    sDescription = sDescription.substring(0, 217) + " ...";
-                } else if (sDescription.length == 0) {
-                    sDescription = "- No description -";
-                }
-                hContainer += "<p>" + sDescription + "</p>";
-                hContainer += "<a type='button' href='"+aData.urls[0].url+"' target='_blank' class='btn' data-character='" + aData.id + "'>VIEW MORE</a>";
-                hContainer += "</div>";
-                hContainer += "</div>";
-
-                // Body
-                hContainer += "<div class='characterBody'>";
-                hContainer += "<h4>Related comics</h4>";
-                // Body Print 4 comincs
-                var nRowComic = 0;
-                $.each(aData.comics.items, function (nIndiceComics, $aDataComics) {
-                    // Open Row Comics
-                    hContainer += (nRowComic++ == 0) ? "<div class='row'>" : "";
-
-                    // Print comics
-                    hContainer += "<div class='col-xs-12 col-sm-6'>";
-                    hContainer += "<a href='#' data-toggle='modal' data-target='#mdlComicCharacter' data-comic='" + $aDataComics.resourceURI + "'>" + $aDataComics.name + "</a>";
-                    hContainer += "</div>";
-
-                    // Close Row Comics
-                    if (nRowComic == 2) {
-                        hContainer += "</div>";
-                        nRowComic = 0;
-                    }
-
-                    if (nIndiceComics == 3)
-                        return false; // Close bucle
-                });
-                // Close Row number odd
-                hContainer += (nRowComic > 0) ? "</div>" : "";
-                hContainer += "</div>"; // End body Comics
-
-                // End Container
-                hContainer += "</div>";
-                hContainer += "</div>";
-
-                // Close Row
-                if (nRow == 2) {
-                    hContainer += "</div>";
-                    nRow = 0;
-                }
-            });
-
-            // Close Row number odd
-            hContainer += (nRow > 0) ? "</div>" : "";
-
-            $("#container-characters").html(hContainer);
-        }).fail(function (err) {
-            console.log(err);
-        });
     }
 
-    function printComics() {
-        // Params Get
-        var sUrl = _url + "public/comics";
-        var aDataGet = {apikey: _publicKey, limit: 4, offset: 0};
-
-
-        // Clear Container Comic
-        $("#container-comics").empty();
-
-        var hContainer = "";
-        $.get(sUrl, aDataGet).done(function (msg) {
-
-            // Run Comic
-            $.each(msg.data.results, function (nIndice, aData) {
-                // Image
-                hContainer += "<div class='row'>";
-                hContainer += "<div class='col-xs-12'>";
-                hContainer += "<img src='assets/icons/btn-delete.png' class='btnDelete' data-comic='" + aData.id + "'/>";
-                hContainer += "<img class='img-responsive' src='" + aData.thumbnail.path + "." + aData.thumbnail.extension + "' alt='comic'>";
-                hContainer += "</div>";
-                hContainer += "</div>";
-                // Description
-                hContainer += "<div class='row'>";
-                hContainer += "<div class='col-xs-12'>";
-                hContainer += "<h3 class='text-center'>" + aData.title + "</h3>";
-                hContainer += "</div>";
-                hContainer += "</div>";
-                hContainer += "<br />";
-            });
-
-            $("#container-comics").html(hContainer);
-
-
-        }).fail(function (err) {
-            alert("Ango anda mal");
-            console.log(err);
-        });
+    function getParameterByName(name, url) {
+        if (!url) {
+            url = window.location.href;
+        }
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+                results = regex.exec(url);
+        if (!results)
+            return null;
+        if (!results[2])
+            return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
     }
-
 });
--- index.php
+++ index.php
@@ -9,212 +9,29 @@
         <link rel="stylesheet" href="assets/global/font-awesome/css/font-awesome.min.css">
         <link rel="stylesheet" href="assets/global/simple-line-icons/simple-line-icons.min.css"/>
         <link rel="stylesheet" href="assets/global/bootstrap-select/bootstrap-select.min.css"/>
+        <link rel="stylesheet" href="assets/global/simplePagination.js-master/simplePagination.css"/>
 
         <!-- Page -->
         <link rel="stylesheet" href="assets/css/components.css"/>
         <link rel="stylesheet" href="assets/css/plugins.css "/>
+        
+        <!-- Page Developer -->
+        <link rel="stylesheet" href="assets/css/marvel.css" />
 
-        <style type="text/css">
-
-            /* Font */
-            @font-face {
-                font-family: TradeGothicBold;
-                src: url(assets/fonts/trade-gothic-bold/TradeGothic-Bold.woff);
-            }
-
-            /* Style */
-            body{
-                background-color: #F4F4F4;
-            }
-
-            h1, h2, h3, h4, h5, h6 {
-                font-family: TradeGothicBold;
-            }
-
-            .form-control{
-                background-color: #fff;
-                border: 1px solid #DCDCDC;
-                border-radius: 0px;
-                box-shadow: none;
-                color: #333;
-                font-size: 14px;
-                font-weight: normal;
-                transition: border-color 0.15s ease-in-out 0s, box-shadow 0.15s ease-in-out 0s;
-            }
-
-            .navbar {
-                margin-bottom: 0px;
-                -webkit-box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.4);
-                -moz-box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.4);
-                box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.4);
-                z-index: 10;
-            }
-            .navbar-default{
-                background-image: inherit;
-                background-color: #2D2727;
-                border: 0px;
-                border-radius: 0px;
-                height: 80px;
-            }
-
-            #logo{
-                width: 170px;
-            }
-
-            .navTop{
-                padding-top: 10px; 
-            }
-
-            #search{
-                padding: 5px;
-            }
-
-            #search input{
-                height: 50px;
-                background-color: #FAFAFA;
-                font-size: 18px;
-                border-radius: 3px;
-            }
-
-            .form-horizontal .has-feedback .form-control-feedback {
-                color: #b5b5b5;
-                font-size: 22pt;
-                right: 15px;
-                top: 22px;
-                vertical-align: middle;
-            }
-
-            #bodyMarvel{
-                display: flex;
-            }
-
-            .section{
-                padding-top: 30px;
-            }
-
-            .titleIcon{
-                margin-top: 3px; 
-                font-weight: bold;
-                font-family: TradeGothicBold;
-            }
-
-            .selectpicker{
-                height: 50px;
-            }
-
-            .cantainer-character{
-                background-color: #FFF;
-                padding: 0 15px;
-                display: block;
-                margin: 20px 0px 20px 20px;
-            }
-
-            .cantainer-character .characterImage{
-                left: -30px;
-                top: -15px;
-                padding: 0px;
-            }
-
-            .cantainer-character .characterImage img{
-                height: 22rem;
-                width: 24rem;
-            }
-
-            .cantainer-character .characterDescription{
-                top: -36px;
-                padding: 0;
-            }
-
-            .cantainer-character p, .cantainer-character a{
-                font-size: 14px;
-                color: #B9B3B3;
-            }
-
-            .cantainer-character .characterDescription a{
-                background-color: #EB1E23;
-                color: #FFF;
-                font-weight: bold;
-            }
-
-            .characterHead{
-                padding: 0 10px;
-                min-height: 260px;
-            }
-
-            .characterBody{
-                position: relative;
-                top: -30px;
-                min-height: 135px;
-            }
-
-            .btnDelete{
-                position: absolute;
-                right: 0;
-                top: -20px;
-            }
-
-            /* Modal */
-            .modal-backdrop.in{
-                opacity: 0.7 !important;
-            }
-            
-            .modal-dialog {
-                width: 700px;
-            }
-            
-            
-            .modal-body{
-                padding: 5px 15px 15px 15px;
-            }
-            
-            .modal-body h2{
-                margin-top: 0px;
-            }
-            
-            .modal-body p{
-                color: #b9b3b3;
-                font-size: 14px;
-            }
-            
-            .modal-footer{
-                display: flex;
-                padding: 0;
-            }
-            
-            .modal-footer button{
-                flex: 1 1 auto;
-                margin: 0 !important;
-                font-weight: bold;
-                padding: 10px 0;
-            }
-            
-            .modal-footer button:nth-child(1){
-                background-color:  #F0F0F0; 
-                color: #3C3332;
-            }
-            .modal-footer button:nth-child(1):hover{
-                background-color:  #322827; 
-                color: #EC1D23;
-            }
-            .modal-footer button:nth-child(2){
-                background-color:  #DCDCDC; 
-                color: #EC1D23;
-            }
-
-        </style>
     </head>
     <body>
 
+        <!-- Head -->
         <nav class="navbar navbar-default">
             <div class="container-fluid">
                 <div class="navTop">
-                    <form class="form-horizontal" role="search">
+                    <form class="form-horizontal" type="get" role="search">
                         <div class="form-group has-feedback">
-                            <div class="col-xs-3">
+                            <div class="col-xs-3 hidden-xs">
                                 <img alt="Marvel" id="logo" src="assets/img/Marvel-logo.png">
                             </div>
-                            <div class="col-xs-6" id="search">
-                                <input type="text" class="form-control" id="inputSuccess3" placeholder="Search character..." aria-describedby="searchMarvel">
+                            <div class="col-sm-6 col-xs-12" id="divSearch">
+                                <input type="text" class="form-control" id="search" name="search" placeholder="Search character..." aria-describedby="searchMarvel">
                                 <span class="icon-magnifier icons form-control-feedback" aria-hidden="true"></span>
                             </div>
                         </div>
@@ -223,6 +40,7 @@
             </div>
         </nav>
 
+        <!-- Body -->
         <div class="container-fuid" id="bodyMarvel">
             <div class="col-md-9 col-sm-12 section">
                 <div class="row">
@@ -245,8 +63,10 @@
                 <br />
                 <div id="container-characters">
                 </div>
+                <div class="row text-center" id="pageNavigation">
+                </div>
             </div>
-            <div class="col-md-3 hidden-sm section" style="background-color: #EBEBEB;">
+            <div class="col-md-3 hidden-xs hidden-sm section" style="background-color: #EBEBEB;">
                 <div class="row">
                     <div class="col-sm-3">
                         <img class="img-responsive center-block" src="assets/icons/favourites.png" alt="favourites">
@@ -301,6 +121,7 @@
         <script type="text/javascript" src="assets/global/angular-1.6.0.min.js"></script>
         <script type="text/javascript" src="assets/global/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="assets/global/bootstrap-select/bootstrap-select.min.js"></script>
+        <script type="text/javascript" src="assets/global/simplePagination.js-master/jquery.simplePagination.js"></script>
 
         <!-- Page -->
         <script type="text/javascript" src="assets/js/jq_marvel.js"></script>
